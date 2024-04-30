75f5a8a211b93ffb3617d84e8b37f8bf9b064e8d
Amaris <20200003@ricaldone.edu.sv>
2024-04-29 20:48:38 -0600

    Primer y ultimo commit

---
+++ app/src/main/java/amaris/osorio/emulador_20200003_amaris/activity_login.kt
@@ -0,0 +1,29 @@
+package amaris.osorio.emulador_20200003_amaris
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class activity_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button = findViewById<Button>(R.id.btn_signin)
+
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/amaris/osorio/emulador_20200003_amaris/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package amaris.osorio.emulador_20200003_amaris.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
